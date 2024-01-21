2b73957276384efc3030e86c65d12f5fd0366e65
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-01-21 00:52:07 -0500

    Actualizacon para resetear el password manejo de funciones separadas y mas cohrentes y logicas

--- dist/controllers/authentication/login/passwordReset/passwordResetEmailController.js
+++ dist/controllers/authentication/login/passwordReset/passwordResetEmailController.js
@@ -23,90 +23,196 @@ const PASSWORD_REGEX_UPPERCASE = /[A-Z]/;
 const PASSWORD_REGEX_LOWERCASE = /[a-z]/;
 const PASSWORD_REGEX_SPECIAL = /[&$@_/-]/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-/////////////////////////////////////////////////////Restablece la contraseña de un usuario.///////////////////////////////////////////////////////////
 /**
- * Restablece la contraseña de un usuario.
- * @param {Request} req - Objeto de solicitud de Express.
- * @param {Response} res - Objeto de respuesta de Express.
- * @returns {Promise<void>} - Responde con un mensaje indicando si la contraseña se restableció con éxito.
+ * Buscar al usuario en la base de datos según el nombre de usuario o correo electrónico.
+ * @param {string} usernameOrEmail - Nombre de usuario o correo electrónico.
+ * @returns {Promise<AuthModel | null>} - Usuario encontrado o nulo si no existe.
  */
+const findUserByUsernameOrEmail = (usernameOrEmail) => __awaiter(void 0, void 0, void 0, function* () {
+    if (EMAIL_REGEX.test(usernameOrEmail)) {
+        return yield authModel_1.Auth.findOne({ where: { email: usernameOrEmail }, include: [verificationModel_1.Verification] });
+    }
+    else {
+        return yield authModel_1.Auth.findOne({ where: { username: usernameOrEmail }, include: [verificationModel_1.Verification] });
+    }
+});
+/**
+ * Verificar si la cuenta del usuario está verificada.
+ * @param {AuthModel} user - Usuario para verificar.
+ * @returns {boolean} - True si la cuenta está verificada, false de lo contrario.
+ */
+const isAccountVerified = (user) => {
+    const verification = user.verification;
+    // Retornar false si no hay registro de verificación o la cuenta no está verificada
+    return verification ? (verification.isEmailVerified && verification.isPhoneVerified) : false;
+};
+/**
+ * Validar la contraseña aleatoria y el tiempo de expiración.
+ * @param {VerificationModel} verification - Registro de verificación.
+ * @param {string} randomPassword - Contraseña aleatoria proporcionada.
+ * @returns {boolean} - True si la contraseña aleatoria y el tiempo son válidos, false de lo contrario.
+ */
+const isRandomPasswordValid = (verification, randomPassword) => {
+    return verification.randomPassword === randomPassword && verification.verificationCodeExpiration >= new Date();
+};
+/**
+ * Validar la longitud mínima de la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si la longitud es insuficiente, nulo si es válida.
+ */
+const validateMinLength = (newPassword) => {
+    return newPassword.length < PASSWORD_MIN_LENGTH ? messages_1.errorMessages.passwordTooShort : null;
+};
+/**
+ * Validar la presencia de al menos un número en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay un número, nulo si es válida.
+ */
+const validateNumber = (newPassword) => {
+    return PASSWORD_REGEX_NUMBER.test(newPassword) ? null : messages_1.errorMessages.passwordNoNumber;
+};
+/**
+ * Validar la presencia de al menos una mayúscula en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay una mayúscula, nulo si es válida.
+ */
+const validateUppercase = (newPassword) => {
+    return PASSWORD_REGEX_UPPERCASE.test(newPassword) ? null : messages_1.errorMessages.passwordNoUppercase;
+};
+/**
+ * Validar la presencia de al menos una minúscula en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay una minúscula, nulo si es válida.
+ */
+const validateLowercase = (newPassword) => {
+    return PASSWORD_REGEX_LOWERCASE.test(newPassword) ? null : messages_1.errorMessages.passwordNoLowercase;
+};
+/**
+ * Validar la presencia de al menos un carácter especial en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay un carácter especial, nulo si es válida.
+ */
+const validateSpecialChar = (newPassword) => {
+    return PASSWORD_REGEX_SPECIAL.test(newPassword) ? null : messages_1.errorMessages.passwordNoSpecialChar;
+};
+/**
+ * Validar la nueva contraseña según las reglas establecidas.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si la contraseña no cumple con las reglas, nulo si es válida.
+ */
+const validateNewPassword = (newPassword) => {
+    const validators = [
+        validateMinLength,
+        validateNumber,
+        validateUppercase,
+        validateLowercase,
+        validateSpecialChar,
+    ];
+    for (const validator of validators) {
+        const errorMessage = validator(newPassword);
+        if (errorMessage !== null) {
+            return errorMessage;
+        }
+    }
+    return null; // La contraseña cumple con todas las reglas.
+};
+/**
+ * Encriptar la nueva contraseña y actualizarla en el usuario.
+ * @param {AuthModel} user - Usuario al que se le actualizará la contraseña.
+ * @param {string} newPassword - Nueva contraseña a encriptar y asignar al usuario.
+ * @returns {Promise<void>} - Resuelve cuando la contraseña se ha actualizado correctamente.
+ */
+const updatePassword = (user, newPassword) => __awaiter(void 0, void 0, void 0, function* () {
+    const hashedPassword = yield bcryptjs_1.default.hash(newPassword, 10);
+    user.password = hashedPassword;
+});
+/**
+ * Limpiar la contraseña aleatoria y actualizar la fecha de expiración en el registro de verificación.
+ * @param {VerificationModel} verification - Registro de verificación al que se le actualizarán los datos.
+ * @returns {Promise<void>} - Resuelve cuando se han actualizado los datos correctamente.
+ */
+const clearRandomPassword = (verification) => __awaiter(void 0, void 0, void 0, function* () {
+    verification.randomPassword = '';
+    verification.verificationCodeExpiration = new Date();
+    yield verification.save();
+});
 const resetPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { usernameOrEmail, randomPassword, newPassword } = req.body;
     try {
-        // Buscar al usuario en la base de datos según el nombre de usuario o correo electrónico
-        let user = null;
-        if (EMAIL_REGEX.test(usernameOrEmail)) {
-            user = yield authModel_1.Auth.findOne({ where: { email: usernameOrEmail }, include: [verificationModel_1.Verification] });
-        }
-        else {
-            user = yield authModel_1.Auth.findOne({ where: { username: usernameOrEmail }, include: [verificationModel_1.Verification] });
-        }
-        // Verificar si el usuario no existe
+        const user = yield findUser(usernameOrEmail);
         if (!user) {
-            return res.status(404).json({
-                msg: messages_1.errorMessages.userNotFound,
-            });
+            return handleResponse(res, 404, messages_1.errorMessages.userNotFound);
         }
-        // Obtener el registro de verificación asociado al usuario
-        const verification = user.verification;
-        // Verificar si la cuenta del usuario no está verificada
-        if (!verification || !verification.isEmailVerified || !verification.isPhoneVerified) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.unverifiedAccount,
-            });
-        }
-        // Verificar si la contraseña aleatoria y el tiempo de expiración son válidos
-        if (verification.randomPassword !== randomPassword || verification.verificationCodeExpiration < new Date()) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.invalidRandomPassword,
-            });
-        }
-        // Validar que la nueva contraseña cumpla con las reglas
-        if (newPassword.length < PASSWORD_MIN_LENGTH) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.passwordTooShort,
-            });
-        }
-        if (!PASSWORD_REGEX_NUMBER.test(newPassword)) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.passwordNoNumber,
-            });
-        }
-        if (!PASSWORD_REGEX_UPPERCASE.test(newPassword)) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.passwordNoUppercase,
-            });
-        }
-        if (!PASSWORD_REGEX_LOWERCASE.test(newPassword)) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.passwordNoLowercase,
-            });
-        }
-        if (!PASSWORD_REGEX_SPECIAL.test(newPassword)) {
-            return res.status(400).json({
-                msg: messages_1.errorMessages.passwordNoSpecialChar,
-            });
-        }
-        // Encriptar la nueva contraseña y actualizarla en el usuario
-        const hashedPassword = yield bcryptjs_1.default.hash(newPassword, 10);
-        user.password = hashedPassword;
-        // Limpiar la contraseña aleatoria y actualizar la fecha de expiración en el registro de verificación
-        verification.randomPassword = '';
-        verification.verificationCodeExpiration = new Date();
-        // Guardar los cambios en el usuario y en el registro de verificación
-        yield Promise.all([user.save(), verification.save()]);
-        // Responder con un mensaje de éxito
-        res.json({
-            msg: messages_1.successMessages.passwordUpdated,
-        });
+        validateAccountAndVerification(user, res, randomPassword, newPassword);
+        yield updateAndClearPassword(user, user.verification, newPassword);
+        res.json({ msg: messages_1.successMessages.passwordUpdated });
     }
     catch (error) {
-        // Manejar errores y responder con un mensaje de error en caso de fallo
-        console.error('Error al resetear la contraseña:', error);
-        res.status(500).json({
-            msg: messages_1.errorMessages.serverError,
-            error: error,
-        });
+        handleServerError(error, res);
     }
 });
 exports.resetPassword = resetPassword;
+const validateAccountAndVerification = (user, res, randomPassword, newPassword) => {
+    validateAccountVerification(user, res);
+    const verification = getVerification(user);
+    validateRandomPasswordAndNewPassword(verification, res, randomPassword, newPassword);
+};
+const validateRandomPasswordAndNewPassword = (verification, res, randomPassword, newPassword) => {
+    if (!verification || !isRandomPasswordValid(verification, randomPassword)) {
+        handleResponse(res, 400, messages_1.errorMessages.invalidRandomPassword);
+        return;
+    }
+    const passwordValidationError = validateNewPassword(newPassword);
+    if (passwordValidationError) {
+        handleResponse(res, 400, passwordValidationError);
+    }
+};
+const handleResponse = (res, statusCode, message) => {
+    res.status(statusCode).json({ msg: message });
+    throw new Error(message);
+};
+const findUser = (usernameOrEmail) => __awaiter(void 0, void 0, void 0, function* () {
+    if (EMAIL_REGEX.test(usernameOrEmail)) {
+        return yield authModel_1.Auth.findOne({ where: { email: usernameOrEmail }, include: [verificationModel_1.Verification] });
+    }
+    else {
+        return yield authModel_1.Auth.findOne({ where: { username: usernameOrEmail }, include: [verificationModel_1.Verification] });
+    }
+});
+const validateUserExistence = (user, res) => {
+    if (!user) {
+        res.status(404).json({ msg: messages_1.errorMessages.userNotFound });
+    }
+};
+const validateAccountVerification = (user, res) => {
+    if (!isAccountVerified(user)) {
+        res.status(400).json({ msg: messages_1.errorMessages.unverifiedAccount });
+    }
+};
+const getVerification = (user) => {
+    return user.verification;
+};
+const validateRandomPassword = (verification, randomPassword, res) => {
+    if (!verification || !isRandomPasswordValid(verification, randomPassword)) {
+        res.status(400).json({ msg: messages_1.errorMessages.invalidRandomPassword });
+    }
+};
+const validatePasswordError = (passwordValidationError, res) => {
+    if (passwordValidationError) {
+        res.status(400).json({ msg: passwordValidationError });
+    }
+};
+const updateAndClearPassword = (user, verification, newPassword) => __awaiter(void 0, void 0, void 0, function* () {
+    const hashedPassword = yield bcryptjs_1.default.hash(newPassword, 10);
+    user.password = hashedPassword;
+    if (verification) {
+        verification.randomPassword = '';
+        verification.verificationCodeExpiration = new Date();
+        yield verification.save();
+    }
+    yield user.save();
+});
+const handleServerError = (error, res) => {
+    console.error('Error al resetear la contraseña:', error);
+    res.status(500).json({ msg: messages_1.errorMessages.serverError, error: error });
+};
--- src/controllers/authentication/login/passwordReset/passwordResetEmailController.ts
+++ src/controllers/authentication/login/passwordReset/passwordResetEmailController.ts
@@ -4,7 +4,6 @@ import { Auth, AuthModel } from '../../../../models/authModel';
 import { Verification, VerificationModel } from '../../../../models/verificationModel';
 import { errorMessages, successMessages } from '../../../../middleware/messages';
 
-
 const PASSWORD_MIN_LENGTH = 10;
 const PASSWORD_REGEX_NUMBER = /\d/;
 const PASSWORD_REGEX_UPPERCASE = /[A-Z]/;
@@ -12,111 +11,233 @@ const PASSWORD_REGEX_LOWERCASE = /[a-z]/;
 const PASSWORD_REGEX_SPECIAL = /[&$@_/-]/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+/**
+ * Buscar al usuario en la base de datos según el nombre de usuario o correo electrónico.
+ * @param {string} usernameOrEmail - Nombre de usuario o correo electrónico.
+ * @returns {Promise<AuthModel | null>} - Usuario encontrado o nulo si no existe.
+ */
+const findUserByUsernameOrEmail = async (usernameOrEmail: string): Promise<AuthModel | null> => {
+    if (EMAIL_REGEX.test(usernameOrEmail)) {
+        return await Auth.findOne({ where: { email: usernameOrEmail }, include: [Verification] });
+    } else {
+        return await Auth.findOne({ where: { username: usernameOrEmail }, include: [Verification] });
+    }
+};
 
-/////////////////////////////////////////////////////Restablece la contraseña de un usuario.///////////////////////////////////////////////////////////
+/**
+ * Verificar si la cuenta del usuario está verificada.
+ * @param {AuthModel} user - Usuario para verificar.
+ * @returns {boolean} - True si la cuenta está verificada, false de lo contrario.
+ */
+const isAccountVerified = (user: AuthModel): boolean => {
+    const verification: VerificationModel | null = (user as any).verification;
 
+    // Retornar false si no hay registro de verificación o la cuenta no está verificada
+    return verification ? (verification.isEmailVerified && verification.isPhoneVerified) : false;
+};
 
 
 /**
- * Restablece la contraseña de un usuario.
- * @param {Request} req - Objeto de solicitud de Express.
- * @param {Response} res - Objeto de respuesta de Express.
- * @returns {Promise<void>} - Responde con un mensaje indicando si la contraseña se restableció con éxito.
+ * Validar la contraseña aleatoria y el tiempo de expiración.
+ * @param {VerificationModel} verification - Registro de verificación.
+ * @param {string} randomPassword - Contraseña aleatoria proporcionada.
+ * @returns {boolean} - True si la contraseña aleatoria y el tiempo son válidos, false de lo contrario.
  */
-export const resetPassword = async (req: Request, res: Response) => {
+const isRandomPasswordValid = (verification: VerificationModel, randomPassword: string): boolean => {
+    return verification.randomPassword === randomPassword && verification.verificationCodeExpiration >= new Date();
+};
+
+/**
+ * Validar la longitud mínima de la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si la longitud es insuficiente, nulo si es válida.
+ */
+const validateMinLength = (newPassword: string): string | null => {
+    return newPassword.length < PASSWORD_MIN_LENGTH ? errorMessages.passwordTooShort : null;
+};
+
+/**
+ * Validar la presencia de al menos un número en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay un número, nulo si es válida.
+ */
+const validateNumber = (newPassword: string): string | null => {
+    return PASSWORD_REGEX_NUMBER.test(newPassword) ? null : errorMessages.passwordNoNumber;
+};
+
+/**
+ * Validar la presencia de al menos una mayúscula en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay una mayúscula, nulo si es válida.
+ */
+const validateUppercase = (newPassword: string): string | null => {
+    return PASSWORD_REGEX_UPPERCASE.test(newPassword) ? null : errorMessages.passwordNoUppercase;
+};
+
+/**
+ * Validar la presencia de al menos una minúscula en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay una minúscula, nulo si es válida.
+ */
+const validateLowercase = (newPassword: string): string | null => {
+    return PASSWORD_REGEX_LOWERCASE.test(newPassword) ? null : errorMessages.passwordNoLowercase;
+};
+
+/**
+ * Validar la presencia de al menos un carácter especial en la contraseña.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si no hay un carácter especial, nulo si es válida.
+ */
+const validateSpecialChar = (newPassword: string): string | null => {
+    return PASSWORD_REGEX_SPECIAL.test(newPassword) ? null : errorMessages.passwordNoSpecialChar;
+};
+
+/**
+ * Validar la nueva contraseña según las reglas establecidas.
+ * @param {string} newPassword - Nueva contraseña a validar.
+ * @returns {string | null} - Mensaje de error si la contraseña no cumple con las reglas, nulo si es válida.
+ */
+const validateNewPassword = (newPassword: string): string | null => {
+    const validators = [
+        validateMinLength,
+        validateNumber,
+        validateUppercase,
+        validateLowercase,
+        validateSpecialChar,
+    ];
+
+    for (const validator of validators) {
+        const errorMessage = validator(newPassword);
+        if (errorMessage !== null) {
+            return errorMessage;
+        }
+    }
+
+    return null; // La contraseña cumple con todas las reglas.
+};
+
+/**
+ * Encriptar la nueva contraseña y actualizarla en el usuario.
+ * @param {AuthModel} user - Usuario al que se le actualizará la contraseña.
+ * @param {string} newPassword - Nueva contraseña a encriptar y asignar al usuario.
+ * @returns {Promise<void>} - Resuelve cuando la contraseña se ha actualizado correctamente.
+ */
+const updatePassword = async (user: AuthModel, newPassword: string): Promise<void> => {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+};
+
+/**
+ * Limpiar la contraseña aleatoria y actualizar la fecha de expiración en el registro de verificación.
+ * @param {VerificationModel} verification - Registro de verificación al que se le actualizarán los datos.
+ * @returns {Promise<void>} - Resuelve cuando se han actualizado los datos correctamente.
+ */
+const clearRandomPassword = async (verification: VerificationModel): Promise<void> => {
+    verification.randomPassword = '';
+    verification.verificationCodeExpiration = new Date();
+    await verification.save();
+};
+
+export const resetPassword = async (req: Request, res: Response): Promise<void> => {
     const { usernameOrEmail, randomPassword, newPassword } = req.body;
 
     try {
+        const user = await findUser(usernameOrEmail);
 
-        // Buscar al usuario en la base de datos según el nombre de usuario o correo electrónico
-        let user: AuthModel | null = null;
-
-        if (EMAIL_REGEX.test(usernameOrEmail)) {
-            user = await Auth.findOne({ where: { email: usernameOrEmail }, include: [Verification] });
-        } else {
-            user = await Auth.findOne({ where: { username: usernameOrEmail }, include: [Verification] });
-        }
-
-        // Verificar si el usuario no existe
         if (!user) {
-            return res.status(404).json({
-                msg: errorMessages.userNotFound,
-            });
+            return handleResponse(res, 404, errorMessages.userNotFound);
         }
 
-        // Obtener el registro de verificación asociado al usuario
-        const verification: VerificationModel | null = (user as any).verification;
+        validateAccountAndVerification(user, res, randomPassword, newPassword);
 
-        // Verificar si la cuenta del usuario no está verificada
-        if (!verification || !verification.isEmailVerified || !verification.isPhoneVerified) {
-            return res.status(400).json({
-                msg: errorMessages.unverifiedAccount,
-            });
-        }
+        await updateAndClearPassword(user, user.verification, newPassword);
 
-        // Verificar si la contraseña aleatoria y el tiempo de expiración son válidos
-        if (verification.randomPassword !== randomPassword || verification.verificationCodeExpiration < new Date()) {
-            return res.status(400).json({
-                msg: errorMessages.invalidRandomPassword,
-            });
-        }
-
-        // Validar que la nueva contraseña cumpla con las reglas
-        if (newPassword.length < PASSWORD_MIN_LENGTH) {
-            return res.status(400).json({
-                msg: errorMessages.passwordTooShort,
-            });
-        }
-
-        if (!PASSWORD_REGEX_NUMBER.test(newPassword)) {
-            return res.status(400).json({
-                msg: errorMessages.passwordNoNumber,
-            });
-        }
-
-        if (!PASSWORD_REGEX_UPPERCASE.test(newPassword)) {
-            return res.status(400).json({
-                msg: errorMessages.passwordNoUppercase,
-            });
-        }
-
-        if (!PASSWORD_REGEX_LOWERCASE.test(newPassword)) {
-            return res.status(400).json({
-                msg: errorMessages.passwordNoLowercase,
-            });
-        }
-
-        if (!PASSWORD_REGEX_SPECIAL.test(newPassword)) {
-            return res.status(400).json({
-                msg: errorMessages.passwordNoSpecialChar,
-            });
-        }
-
-        // Encriptar la nueva contraseña y actualizarla en el usuario
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        user.password = hashedPassword;
-
-        // Limpiar la contraseña aleatoria y actualizar la fecha de expiración en el registro de verificación
-        verification.randomPassword = '';
-        verification.verificationCodeExpiration = new Date();
-
-        // Guardar los cambios en el usuario y en el registro de verificación
-        await Promise.all([user.save(), verification.save()]);
-
-        // Responder con un mensaje de éxito
-        res.json({
-            msg: successMessages.passwordUpdated,
-        });
+        res.json({ msg: successMessages.passwordUpdated });
     } catch (error) {
-
-        // Manejar errores y responder con un mensaje de error en caso de fallo
-        console.error('Error al resetear la contraseña:', error);
-        res.status(500).json({
-            msg: errorMessages.serverError,
-            error: error,
-        });
+        handleServerError(error, res);
     }
+};
+
+const validateAccountAndVerification = (user: AuthModel, res: Response, randomPassword: string, newPassword: string): void => {
+    validateAccountVerification(user, res);
+
+    const verification = getVerification(user);
+    validateRandomPasswordAndNewPassword(verification, res, randomPassword, newPassword);
+};
+
+const validateRandomPasswordAndNewPassword = (verification: VerificationModel | null, res: Response, randomPassword: string, newPassword: string): void => {
+    if (!verification || !isRandomPasswordValid(verification, randomPassword)) {
+        handleResponse(res, 400, errorMessages.invalidRandomPassword);
+        return;
+    }
+
+    const passwordValidationError = validateNewPassword(newPassword);
+    if (passwordValidationError) {
+        handleResponse(res, 400, passwordValidationError);
+    }
+};
+
+const handleResponse = (res: Response, statusCode: number, message: string): void => {
+    res.status(statusCode).json({ msg: message });
+    throw new Error(message);
 };
 
 
 
+const findUser = async (usernameOrEmail: string): Promise<AuthModel | null> => {
+    if (EMAIL_REGEX.test(usernameOrEmail)) {
+        return await Auth.findOne({ where: { email: usernameOrEmail }, include: [Verification] });
+    } else {
+        return await Auth.findOne({ where: { username: usernameOrEmail }, include: [Verification] });
+    }
+};
+
+const validateUserExistence = (user: AuthModel | null, res: Response): void => {
+    if (!user) {
+        res.status(404).json({ msg: errorMessages.userNotFound });
+        
+    }
+};
+
+const validateAccountVerification = (user: AuthModel, res: Response): void => {
+    if (!isAccountVerified(user)) {
+        res.status(400).json({ msg: errorMessages.unverifiedAccount });
+       
+    }
+};
+
+const getVerification = (user: AuthModel): VerificationModel | null => {
+    return (user as any).verification;
+};
+
+const validateRandomPassword = (verification: VerificationModel | null, randomPassword: string, res: Response): void => {
+    if (!verification || !isRandomPasswordValid(verification, randomPassword)) {
+        res.status(400).json({ msg: errorMessages.invalidRandomPassword });
+       
+    }
+};
+
+const validatePasswordError = (passwordValidationError: string | null, res: Response): void => {
+    if (passwordValidationError) {
+        res.status(400).json({ msg: passwordValidationError });
+       
+    }
+};
+
+const updateAndClearPassword = async (user: AuthModel, verification: VerificationModel | null, newPassword: string): Promise<void> => {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+
+    if (verification) {
+        verification.randomPassword = '';
+        verification.verificationCodeExpiration = new Date();
+        await verification.save();
+    }
+
+    await user.save();
+};
+
+const handleServerError = (error: any, res: Response): void => {
+    console.error('Error al resetear la contraseña:', error);
+    res.status(500).json({ msg: errorMessages.serverError, error: error });
+};
