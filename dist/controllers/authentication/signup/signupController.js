2eef2a2e35f6705c469ec2a0f6ec893b588f6143
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-01-21 21:24:31 -0500

    Actualizacion de la funcion de crear un usaurio ya esta factorizada y funcionando vs1

--- dist/controllers/authentication/signup/signupController.js
+++ dist/controllers/authentication/signup/signupController.js
@@ -6,11 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.newUser = void 0;
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const authModel_1 = require("../../../models/authModel");
+const profileAdminModel_1 = require("../../../models/profileAdminModel");
+const verificationModel_1 = require("../../../models/verificationModel");
 const messages_1 = require("../../../middleware/messages");
 const emailVerificationController_1 = require("../email/emailVerificationController");
 const generateCode_1 = require("../../../utils/generateCode");
-const verificationModel_1 = require("../../../models/verificationModel");
-const profileAdminModel_1 = require("../../../models/profileAdminModel");
 const PASSWORD_MIN_LENGTH = 10;
 const PASSWORD_REGEX_NUMBER = /\d/;
 const PASSWORD_REGEX_UPPERCASE = /[A-Z]/;
@@ -18,104 +18,162 @@ const PASSWORD_REGEX_LOWERCASE = /[a-z]/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const VERIFICATION_CODE_EXPIRATION_HOURS = 24;
 const newUser = async (req, res) => {
-    const { username, password, email, rol } = req.body;
-    //1. Verificar que todos los campos obligatorios estén presentes en la solicitud
-    if (!username || !password || !email || !rol) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.requiredFields,
-        });
-    }
-    //2. Validar que la contraseña tenga al menos 8 caracteres
-    if (password.length < PASSWORD_MIN_LENGTH) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.passwordTooShort,
-        });
-    }
-    //3. Validar que la contraseña contenga al menos un número
-    if (!PASSWORD_REGEX_NUMBER.test(password)) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.passwordNoNumber,
-        });
-    }
-    //4. Validar que la contraseña contenga al menos una letra mayúscula
-    if (!PASSWORD_REGEX_UPPERCASE.test(password)) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.passwordNoUppercase,
-        });
-    }
-    //5. Validar que la contraseña contenga al menos una letra minúscula
-    if (!PASSWORD_REGEX_LOWERCASE.test(password)) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.passwordNoLowercase,
-        });
-    }
-    // 6. Validar que la dirección de correo electrónico sea válida
-    if (!EMAIL_REGEX.test(email)) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.invalidEmail,
-        });
-    }
-    //7. Validamos si el usuario ya existe en la base de datos
-    const user = await authModel_1.Auth.findOne({ where: { username: username } });
-    if (user) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.userExists(username),
-        });
-    }
-    //7. Validamos si el usuario ya existe en la base de datos
-    const useremail = await authModel_1.Auth.findOne({ where: { email: email } });
-    if (useremail) {
-        return res.status(400).json({
-            msg: messages_1.errorMessages.userEmailExists(email),
-        });
-    }
-    const hashedPassword = await bcryptjs_1.default.hash(password, 10);
-    //8. Generar el código de verificación único
     try {
-        // Generar el código de verificación único
-        const verificationCode = (0, generateCode_1.generateVerificationCode)();
-        //9. Calcular la fecha de expiración (agregar 24 horas a la fecha actual)
-        const expirationDate = new Date();
-        expirationDate.setHours(expirationDate.getHours() + VERIFICATION_CODE_EXPIRATION_HOURS);
-        //10. Guardar usuario en la base de datos con el código de verificación y fecha de expiración
-        const newUser = await authModel_1.Auth.create({
-            username: username,
-            password: hashedPassword,
-            email: email,
-            rol: rol,
-        });
-        await profileAdminModel_1.UserProfile.create({
-            userId: newUser.id, // Asociar con el ID del usuario creado
-            firstName: '', // Aquí puedes definir los valores iniciales que quieras para el perfil
-            lastName: '',
-            // Otros campos del perfil que desees inicializar
-        });
-        // Crear la entrada de verificación asociada al usuario
-        await verificationModel_1.Verification.create({
-            isVerified: false,
-            verificationCode: verificationCode,
-            verificationCodeExpiration: expirationDate,
-            userId: newUser.id, // Usar el ID del usuario recién creado
-        });
-        // Enviar el código de verificación por correo electrónico
-        const verificationEmailSent = await (0, emailVerificationController_1.sendVerificationEmail)(email, username, verificationCode);
-        //15. Crear el mensaje de respuesta basado en el rol del usuario
-        let userMessage = '';
-        if (rol === 'admin') {
-            userMessage = 'administrador';
-        }
-        else if (rol === 'user') {
-            userMessage = 'normal';
-        }
+        const { username, password, email, rol } = req.body;
+        validateInput(username, password, email, rol);
+        const passwordValidationErrors = validatePasswordRequirements(password);
+        handlePasswordValidationErrors(passwordValidationErrors, res);
+        validateEmail(email);
+        const existingUserError = await checkExistingUser(username, email);
+        handleExistingUserError(existingUserError, res);
+        const hashedPassword = await bcryptjs_1.default.hash(password, 10);
+        const newUser = await createNewUser(username, hashedPassword, email, rol);
+        await initializeUserProfile(newUser.id);
+        const verificationCode = await generateAndSaveVerificationCode(newUser.id, email);
+        await (0, emailVerificationController_1.sendVerificationEmail)(email, username, verificationCode);
+        const userMessage = getRoleMessage(rol);
         res.json({
             msg: messages_1.successMessages.userRegistered(username, userMessage),
         });
     }
     catch (error) {
+        handleServerError(error, res);
+    }
+};
+exports.newUser = newUser;
+const validateInput = (username, password, email, rol) => {
+    const requiredFields = [username, password, email, rol];
+    if (requiredFields.some(field => !field)) {
+        throw new Error(messages_1.errorMessages.requiredFields);
+    }
+};
+const handlePasswordValidationErrors = (errors, res) => {
+    if (errors.length > 0) {
+        res.status(400).json({
+            msg: 'Error en la validación de la contraseña',
+            errors: errors,
+        });
+    }
+};
+const handleExistingUserError = (error, res) => {
+    if (error) {
+        res.status(400).json({
+            msg: error,
+        });
+    }
+};
+const handleServerError = (error, res) => {
+    console.error("Error en el controlador newUser:", error);
+    // Verificar si ya se ha enviado una respuesta
+    if (!res.headersSent) {
         res.status(400).json({
             msg: messages_1.errorMessages.databaseError,
             error,
         });
     }
 };
-exports.newUser = newUser;
+// Cambios en validatePasswordRequirements
+const validatePasswordRequirements = (password) => {
+    const errors = [];
+    validateLength(password, errors);
+    validateNumber(password, errors);
+    validateUppercase(password, errors);
+    validateLowercase(password, errors);
+    return errors;
+};
+const validateLength = (password, errors) => {
+    if (password.length < PASSWORD_MIN_LENGTH) {
+        errors.push(messages_1.errorMessages.passwordTooShort);
+    }
+};
+const validateNumber = (password, errors) => {
+    if (!PASSWORD_REGEX_NUMBER.test(password)) {
+        errors.push(messages_1.errorMessages.passwordNoNumber);
+    }
+};
+const validateUppercase = (password, errors) => {
+    if (!PASSWORD_REGEX_UPPERCASE.test(password)) {
+        errors.push(messages_1.errorMessages.passwordNoUppercase);
+    }
+};
+const validateLowercase = (password, errors) => {
+    if (!PASSWORD_REGEX_LOWERCASE.test(password)) {
+        errors.push(messages_1.errorMessages.passwordNoLowercase);
+    }
+};
+const validateEmail = (email) => {
+    if (!EMAIL_REGEX.test(email)) {
+        throw new Error(messages_1.errorMessages.invalidEmail);
+    }
+};
+const findExistingUsername = async (username) => {
+    try {
+        const existingUsername = await authModel_1.Auth.findOne({ where: { username } });
+        return Boolean(existingUsername);
+    }
+    catch (error) {
+        console.error("Error en findExistingUsername:", error);
+        throw messages_1.errorMessages.databaseError;
+    }
+};
+const findExistingEmail = async (email) => {
+    try {
+        const existingEmail = await authModel_1.Auth.findOne({ where: { email } });
+        return Boolean(existingEmail);
+    }
+    catch (error) {
+        console.error("Error en findExistingEmail:", error);
+        throw messages_1.errorMessages.databaseError;
+    }
+};
+const checkExistingUsername = async (username) => {
+    return (await findExistingUsername(username))
+        ? messages_1.errorMessages.userExists(username)
+        : null;
+};
+const checkExistingEmail = async (email) => {
+    return (await findExistingEmail(email))
+        ? messages_1.errorMessages.userEmailExists(email)
+        : null;
+};
+const checkExistingUser = async (username, email) => {
+    return ((await checkExistingUsername(username)) ||
+        (await checkExistingEmail(email)) ||
+        null);
+};
+const createNewUser = async (username, hashedPassword, email, rol) => {
+    try {
+        return await authModel_1.Auth.create({
+            username: username,
+            password: hashedPassword,
+            email: email,
+            rol: rol,
+        });
+    }
+    catch (error) {
+        console.error("Error en createNewUser:", error);
+        throw messages_1.errorMessages.databaseError; // Propagar el error a la función llamadora
+    }
+};
+const initializeUserProfile = async (userId) => {
+    await profileAdminModel_1.UserProfile.create({
+        userId: userId,
+        firstName: '',
+        lastName: '',
+    });
+};
+const generateAndSaveVerificationCode = async (userId, email) => {
+    const verificationCode = (0, generateCode_1.generateVerificationCode)();
+    const expirationDate = new Date();
+    expirationDate.setHours(expirationDate.getHours() + VERIFICATION_CODE_EXPIRATION_HOURS);
+    await verificationModel_1.Verification.create({
+        isVerified: false,
+        verificationCode: verificationCode,
+        verificationCodeExpiration: expirationDate,
+        userId: userId,
+    });
+    return verificationCode;
+};
+const getRoleMessage = (rol) => {
+    return rol === 'admin' ? 'administrador' : rol === 'user' ? 'normal' : '';
+};
--- src/controllers/authentication/signup/signupController.ts
+++ src/controllers/authentication/signup/signupController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { Auth } from '../../../models/authModel';
+import { UserProfile } from '../../../models/profileAdminModel';
+import { Verification } from '../../../models/verificationModel';
 import { errorMessages, successMessages } from '../../../middleware/messages';
 import { sendVerificationEmail } from '../email/emailVerificationController';
 import { generateVerificationCode } from '../../../utils/generateCode';
-import { Verification } from '../../../models/verificationModel';
-import { UserProfile } from '../../../models/profileAdminModel';
-
 
 const PASSWORD_MIN_LENGTH = 10;
 const PASSWORD_REGEX_NUMBER = /\d/;
@@ -15,120 +14,206 @@ const PASSWORD_REGEX_LOWERCASE = /[a-z]/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const VERIFICATION_CODE_EXPIRATION_HOURS = 24;
 
-
 export const newUser = async (req: Request, res: Response) => {
-  const { username, password, email, rol } = req.body;
-
-  //1. Verificar que todos los campos obligatorios estén presentes en la solicitud
-  if (!username || !password || !email || !rol) {
-    return res.status(400).json({
-      msg: errorMessages.requiredFields,
-    });
-  }
-
-  //2. Validar que la contraseña tenga al menos 8 caracteres
-  if (password.length < PASSWORD_MIN_LENGTH) {
-    return res.status(400).json({
-      msg: errorMessages.passwordTooShort,
-    });
-  }
-
-  //3. Validar que la contraseña contenga al menos un número
-  if (!PASSWORD_REGEX_NUMBER.test(password)) {
-    return res.status(400).json({
-      msg: errorMessages.passwordNoNumber,
-    });
-  }
-
-  //4. Validar que la contraseña contenga al menos una letra mayúscula
-  if (!PASSWORD_REGEX_UPPERCASE.test(password)) {
-    return res.status(400).json({
-      msg: errorMessages.passwordNoUppercase,
-    });
-  }
-
-  //5. Validar que la contraseña contenga al menos una letra minúscula
-  if (!PASSWORD_REGEX_LOWERCASE.test(password)) {
-    return res.status(400).json({
-      msg: errorMessages.passwordNoLowercase,
-    });
-  }
-
-  // 6. Validar que la dirección de correo electrónico sea válida
-  if (!EMAIL_REGEX.test(email)) {
-    return res.status(400).json({
-      msg: errorMessages.invalidEmail,
-    });
-  }
-
-  //7. Validamos si el usuario ya existe en la base de datos
-  const user = await Auth.findOne({ where: { username: username } });
-
-  if (user) {
-    return res.status(400).json({
-      msg: errorMessages.userExists(username),
-    });
-  }
-  //7. Validamos si el usuario ya existe en la base de datos
-  const useremail = await Auth.findOne({ where: { email: email } });
-
-  if (useremail) {
-    return res.status(400).json({
-      msg: errorMessages.userEmailExists(email),
-    });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  //8. Generar el código de verificación único
   try {
-    // Generar el código de verificación único
-    const verificationCode = generateVerificationCode();
+    const { username, password, email, rol } = req.body;
 
-    //9. Calcular la fecha de expiración (agregar 24 horas a la fecha actual)
-    const expirationDate = new Date();
-    expirationDate.setHours(expirationDate.getHours() + VERIFICATION_CODE_EXPIRATION_HOURS);
+    validateInput(username, password, email, rol);
 
-    //10. Guardar usuario en la base de datos con el código de verificación y fecha de expiración
-    const newUser = await Auth.create({
-      username: username,
-      password: hashedPassword,
-      email: email,
-      rol: rol,
-  });
+    const passwordValidationErrors = validatePasswordRequirements(password);
+    handlePasswordValidationErrors(passwordValidationErrors, res);
 
-  await UserProfile.create({
-    userId: newUser.id, // Asociar con el ID del usuario creado
-    firstName: '', // Aquí puedes definir los valores iniciales que quieras para el perfil
-    lastName: '',
-    // Otros campos del perfil que desees inicializar
-  });
+    validateEmail(email);
 
-    // Crear la entrada de verificación asociada al usuario
-    await Verification.create({
-      isVerified: false,
-      verificationCode: verificationCode, 
-      verificationCodeExpiration: expirationDate,
-      userId: newUser.id, // Usar el ID del usuario recién creado
-    });
-    // Enviar el código de verificación por correo electrónico
-    const verificationEmailSent = await sendVerificationEmail(email, username, verificationCode);
+    const existingUserError = await checkExistingUser(username, email);
+    handleExistingUserError(existingUserError, res);
 
-    //15. Crear el mensaje de respuesta basado en el rol del usuario
-    let userMessage = '';
-    if (rol === 'admin') {
-      userMessage = 'administrador';
-    } else if (rol === 'user') {
-      userMessage = 'normal';
-    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await createNewUser(username, hashedPassword, email, rol);
+
+    await initializeUserProfile(newUser.id);
+
+    const verificationCode = await generateAndSaveVerificationCode(newUser.id, email);
+
+    await sendVerificationEmail(email, username, verificationCode);
+
+    const userMessage = getRoleMessage(rol);
 
     res.json({
       msg: successMessages.userRegistered(username, userMessage),
     });
   } catch (error) {
+    handleServerError(error, res);
+  }
+};
+
+const validateInput = (username: string, password: string, email: string, rol: string) => {
+  const requiredFields = [username, password, email, rol];
+  if (requiredFields.some(field => !field)) {
+    throw new Error(errorMessages.requiredFields);
+  }
+};
+
+const handlePasswordValidationErrors = (errors: string[], res: Response) => {
+  if (errors.length > 0) {
+    res.status(400).json({
+      msg: 'Error en la validación de la contraseña',
+      errors: errors,
+    });
+  }
+};
+
+const handleExistingUserError = (error: string | null, res: Response) => {
+  if (error) {
+    res.status(400).json({
+      msg: error,
+    });
+  }
+};
+
+const handleServerError = (error: any, res: Response) => {
+  console.error("Error en el controlador newUser:", error);
+
+  // Verificar si ya se ha enviado una respuesta
+  if (!res.headersSent) {
     res.status(400).json({
       msg: errorMessages.databaseError,
       error,
     });
   }
+};
+
+
+
+
+
+
+// Cambios en validatePasswordRequirements
+const validatePasswordRequirements = (password: string): string[] => {
+  const errors: string[] = [];
+
+  validateLength(password, errors);
+  validateNumber(password, errors);
+  validateUppercase(password, errors);
+  validateLowercase(password, errors);
+
+  return errors;
+};
+
+const validateLength = (password: string, errors: string[]) => {
+  if (password.length < PASSWORD_MIN_LENGTH) {
+    errors.push(errorMessages.passwordTooShort);
+  }
+};
+
+const validateNumber = (password: string, errors: string[]) => {
+  if (!PASSWORD_REGEX_NUMBER.test(password)) {
+    errors.push(errorMessages.passwordNoNumber);
+  }
+};
+
+const validateUppercase = (password: string, errors: string[]) => {
+  if (!PASSWORD_REGEX_UPPERCASE.test(password)) {
+    errors.push(errorMessages.passwordNoUppercase);
+  }
+};
+
+const validateLowercase = (password: string, errors: string[]) => {
+  if (!PASSWORD_REGEX_LOWERCASE.test(password)) {
+    errors.push(errorMessages.passwordNoLowercase);
+  }
+};
+
+const validateEmail = (email: string) => {
+  if (!EMAIL_REGEX.test(email)) {
+    throw new Error(errorMessages.invalidEmail);
+  }
+};
+
+
+
+
+const findExistingUsername = async (username: string): Promise<boolean> => {
+  try {
+    const existingUsername = await Auth.findOne({ where: { username } });
+    return Boolean(existingUsername);
+  } catch (error) {
+    console.error("Error en findExistingUsername:", error);
+    throw errorMessages.databaseError;
+  }
+};
+
+const findExistingEmail = async (email: string): Promise<boolean> => {
+  try {
+    const existingEmail = await Auth.findOne({ where: { email } });
+    return Boolean(existingEmail);
+  } catch (error) {
+    console.error("Error en findExistingEmail:", error);
+    throw errorMessages.databaseError;
+  }
+};
+
+const checkExistingUsername = async (username: string): Promise<string | null> => {
+  return (await findExistingUsername(username))
+    ? errorMessages.userExists(username)
+    : null;
+};
+
+const checkExistingEmail = async (email: string): Promise<string | null> => {
+  return (await findExistingEmail(email))
+    ? errorMessages.userEmailExists(email)
+    : null;
+};
+
+const checkExistingUser = async (username: string, email: string): Promise<string | null> => {
+  return (
+    (await checkExistingUsername(username)) ||
+    (await checkExistingEmail(email)) ||
+    null
+  );
+};
+
+
+
+const createNewUser = async (username: string, hashedPassword: string, email: string, rol: string) => {
+  try {
+    return await Auth.create({
+      username: username,
+      password: hashedPassword,
+      email: email,
+      rol: rol,
+    });
+  } catch (error) {
+    console.error("Error en createNewUser:", error);
+    throw errorMessages.databaseError; // Propagar el error a la función llamadora
+  }
+};
+
+const initializeUserProfile = async (userId: number) => {
+  await UserProfile.create({
+    userId: userId,
+    firstName: '',
+    lastName: '',
+  });
+};
+
+const generateAndSaveVerificationCode = async (userId: number, email: string) => {
+  const verificationCode = generateVerificationCode();
+  const expirationDate = new Date();
+  expirationDate.setHours(expirationDate.getHours() + VERIFICATION_CODE_EXPIRATION_HOURS);
+
+  await Verification.create({
+    isVerified: false,
+    verificationCode: verificationCode,
+    verificationCodeExpiration: expirationDate,
+    userId: userId,
+  });
+
+  return verificationCode;
+};
+
+const getRoleMessage = (rol: string) => {
+  return rol === 'admin' ? 'administrador' : rol === 'user' ? 'normal' : '';
 };
 
